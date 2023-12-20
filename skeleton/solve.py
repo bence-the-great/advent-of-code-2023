029d1d5a3a73e940567df574863a8e7e08bc54fe
Bence Szabó <bence.szabo.92@gmail.com>
2023-12-20 18:12:06 +0100

    feat: modify skeleton solve function to return int

--- skeleton/solve.py
+++ skeleton/solve.py
@@ -1,9 +1,9 @@
 from inputs import real_input, test_input  # noqa: F401
 
 
-def solve() -> None:
-    pass
+def solve(raw_input: str) -> int:
+    return 0
 
 
 if __name__ == "__main__":
-    solve()
+    print(f"{solve(test_input) = }")
